f822326d2dbdd39861de5af750f784250d9a815a
Liz Steak <lizmaepark@gmail.com>
2018-09-19 08:26:54 -0600

    object-ception

--- javascript/datastructuresapp.js
+++ javascript/datastructuresapp.js
@@ -72,82 +72,82 @@
 
 
 // 3. Crazy Object!
-const crazyObject = {
-  taco: [
-          {
-            meat: 'steak',
-            cheese: ['panela', 'queso', 'chihuahua']
-          },
-          {
-            meat: 'chicken',
-            salsa: ["pico", "hot", "hotter", "really hot", "really really hot", 
-                    "omg my mouth is burning"] //5
-          },
-        ],
-  larry: {
-    nicknames: ["LD", "Chicken Teriyaki Boyyyyyy"],
-    quotes: ["Pretty pretty prettayyyyy good", "Is that a parkinson's thing?", 
-             "women love a self confident bald man", "I'm a walking talking enigma"],
-    characters: [
-      {
-        name: "Jeff",
-        occupation: "manager"
-      },
-      {
-        name: "funkhauser",
-        occupation: "tv dude"
-      },
-      {
-        name: "susie",
-        occupation: "jeffs wife",
-        favourtieHobby: "Swearing at Larry and Jeff"
-      },
-    ]
+// const crazyObject = {
+//   taco: [
+//           {
+//             meat: 'steak',
+//             cheese: ['panela', 'queso', 'chihuahua']
+//           },
+//           {
+//             meat: 'chicken',
+//             salsa: ["pico", "hot", "hotter", "really hot", "really really hot", 
+//                     "omg my mouth is burning"] //5
+//           },
+//         ],
+//   larry: {
+//     nicknames: ["LD", "Chicken Teriyaki Boyyyyyy"],
+//     quotes: ["Pretty pretty prettayyyyy good", "Is that a parkinson's thing?", 
+//              "women love a self confident bald man", "I'm a walking talking enigma"],
+//     characters: [
+//       {
+//         name: "Jeff",
+//         occupation: "manager"
+//       },
+//       {
+//         name: "funkhauser",
+//         occupation: "tv dude"
+//       },
+//       {
+//         name: "susie",
+//         occupation: "jeffs wife",
+//         favourtieHobby: "Swearing at Larry and Jeff"
+//       },
+//     ]
+//   }
+// }
+
+// Use crazyObject to log the following
+// "omg my mouth is burning"
+// console.log(crazyObject.taco[1].salsa[5]);
+
+// "Pretty pretty prettayyyyy good"
+// console.log(crazyObject.larry.quotes[0]);
+
+// "Swearing at Larry and Jeff"
+// console.log(crazyObject.larry.characters[2].favourtieHobby);
+
+// "Chicken Teriyaki Boyyyyyy"
+// console.log(crazyObject.larry.nicknames[1]);
+
+// The object the contains the name funkhauser
+// console.log(crazyObject.larry.characters[1]);
+
+// 🔴 Commit: "crazyObject"
+
+
+
+const inception = {
+  reality: {
+      dreamLayer1: {
+          dreamLayer2: {
+              dreamLayer3: {
+                  dreamLayer4: {
+                      dreamLayer5: {
+                          dreamLayer6: {
+                              limbo: "Joseph Gordon Levitt"
+                         }
+                      }
+                  }
+              }
+          }
+      }
   }
 }
 
-Use crazyObject to log the following
-"omg my mouth is burning"
-console.log(crazyObject.taco[1].salsa[5]);
+Change the value of limbo to null. This is changing JGL to Null
+console.log(inception.reality.dreamLayer1.dreamLayer2.dreamLayer3.dreamLayer4.dreamLayer5.dreamLayer6.limbo = 'null');
 
-"Pretty pretty prettayyyyy good"
-console.log(crazyObject.larry.quotes[0]);
-
-"Swearing at Larry and Jeff"
-console.log(crazyObject.larry.characters[2].favourtieHobby);
-
-"Chicken Teriyaki Boyyyyyy"
-console.log(crazyObject.larry.nicknames[1]);
-
-The object the contains the name funkhauser
-console.log(crazyObject.larry.characters[1]);
-
-🔴 Commit: "crazyObject"
-
-
-
-// const inception = {
-//   reality: {
-//   dreamLayer1: {
-//   dreamLayer2: {
-//   dreamLayer3: {
-//   dreamLayer4: {
-//   dreamLayer5: {
-//   dreamLayer6: {
-//   limbo: "Joseph Gordon Levitt"
-//                          }
-//                       }
-//                   }
-//               }
-//           }
-//       }
-//   }
-// } //7
-// Change the value of limbo to null. This is changing JGL to Null
-// inception.limbo = 'null';
-// console.log(inception);
-
-// 🔴 Commit: "object-ception"
+🔴 Commit: "object-ception"
 
 // 5-7. Bond Films
 // Copy the following bondFilms array of objects above into your js file. 
